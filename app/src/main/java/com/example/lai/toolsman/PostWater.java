5de505d1c0a17cfea56337fe4612a39c9c518934
blodbold <41230725+blodbold@users.noreply.github.com>
2018-07-19 00:53:33 +0800

    失敗的圖片上傳
    :(

--- app/src/main/java/com/example/lai/toolsman/PostWater.java
+++ app/src/main/java/com/example/lai/toolsman/PostWater.java
@@ -2,6 +2,7 @@ package com.example.lai.toolsman;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -17,10 +19,12 @@ public class PostWater extends AppCompatActivity {
 
     private DatabaseReference mDatabase;
     private ProgressDialog mProgress;
+    private static final int GALLERY_REQUEST = 1;
 
     private EditText mPostTitle;
     private EditText mPostDesc;
     private Button mSubmitBtn;
+    private ImageButton mSelectImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +37,7 @@ public class PostWater extends AppCompatActivity {
         mPostTitle = (EditText) findViewById(R.id.titleField);
         mPostDesc = (EditText) findViewById(R.id.descField);
         mSubmitBtn = (Button) findViewById(R.id.PostWater);
+        mSelectImage = (ImageButton) findViewById(R.id.imageSelect);
 
         mSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,9 +48,27 @@ public class PostWater extends AppCompatActivity {
                 startActivity(Toelec);
             }
         });
+        mSelectImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
 
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            mSelectImage.setImageURI(imageUri);
+        }
     }
 
     private void startPosting() {
