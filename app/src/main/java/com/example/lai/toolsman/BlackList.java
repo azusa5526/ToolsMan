4ceffb573770479d7b1125d3f112aab4a64e4861
marinlai <41216902+marinlai@users.noreply.github.com>
2018-07-25 10:30:56 +0800

    新增幾行程式碼
    未完成

--- app/src/main/java/com/example/lai/toolsman/BlackList.java
+++ app/src/main/java/com/example/lai/toolsman/BlackList.java
@@ -9,14 +9,18 @@ import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -35,6 +39,7 @@ public class BlackList extends AppCompatActivity {
 
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +53,12 @@ public class BlackList extends AppCompatActivity {
          mDataBase=FirebaseDatabase.getInstance().getReference().child("BlackList");
 
 
+
+
         AddBlackList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                   add();
-
                 }
         });
 
@@ -66,6 +72,8 @@ public class BlackList extends AppCompatActivity {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 newPost.child("Email").setValue(BlackEmail);
+                Toast toast = Toast.makeText(BlackList.this, "添加完成", Toast.LENGTH_LONG);
+                toast.show();
             }
 
             @Override
