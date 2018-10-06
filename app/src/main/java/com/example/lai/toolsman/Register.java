c74281dca632f1436fb7a78e05ab6f857e46d62c
tony5553732006 <41263478+tony5553732006@users.noreply.github.com>
2018-10-06 16:08:28 +0800

    register
    register

--- app/src/main/java/com/example/lai/toolsman/Register.java
+++ app/src/main/java/com/example/lai/toolsman/Register.java
@@ -27,7 +27,7 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        getSupportActionBar().hide();
+
 
         Button Submit = findViewById(R.id.Submit);
         final EditText EmailRegister =(EditText) findViewById(R.id.EmailRegister);
