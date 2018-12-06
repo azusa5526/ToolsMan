32ac70ce7678edb68576c005e48f4e0c7791fdda
andrew <azusa5526@gmail.com>
2018-12-07 03:05:19 +0800

    對話顯示到Chat laout完成

--- app/src/main/java/com/example/lai/toolsman/ChatFunction/ChatsFragment.java
+++ app/src/main/java/com/example/lai/toolsman/ChatFunction/ChatsFragment.java
@@ -1,19 +1,50 @@
 package com.example.lai.toolsman.ChatFunction;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.lai.toolsman.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 
 public class ChatsFragment extends Fragment {
+
+    private RecyclerView mConvList;
+
+    private DatabaseReference mConvDatabase;
+    private DatabaseReference mMessageDatabase;
+    private DatabaseReference mUsersDatabase;
+
+    private FirebaseAuth mAuth;
+
+    private String mCurrentUserId;
+
+    private View mMainView;
 
 
     public ChatsFragment() {
@@ -22,13 +53,159 @@ public class ChatsFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        mMainView = inflater.inflate(R.layout.fragment_chats, container, false);
 
+        mConvList = (RecyclerView) mMainView.findViewById(R.id.convList);
+        mAuth = FirebaseAuth.getInstance();
+
+        mCurrentUserId = mAuth.getCurrentUser().getUid();
+        mConvDatabase = FirebaseDatabase.getInstance().getReference().child("Chat").child(mCurrentUserId);
+
+        mConvDatabase.keepSynced(true);
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+        mMessageDatabase = FirebaseDatabase.getInstance().getReference().child("messages").child(mCurrentUserId);
+        mUsersDatabase.keepSynced(true);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+
+        mConvList.setHasFixedSize(true);
+        mConvList.setLayoutManager(linearLayoutManager);
+
+        return mMainView;
 
     }
+
+    public void onStart() {
+
+        super.onStart();
+
+        Query conversationQuery = mConvDatabase.orderByChild("timestamp");
+
+        FirebaseRecyclerAdapter<Conv, ConvViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Conv, ConvViewHolder>(
+                Conv.class,
+                R.layout.user_list_layout,
+                ConvViewHolder.class,
+                conversationQuery
+        ) {
+            @Override
+            protected void populateViewHolder(final ConvViewHolder convViewHolder, final Conv conv, int i) {
+
+                final String listUserId = getRef(i).getKey();
+                Query lastMessageQuery = mMessageDatabase.child(listUserId).limitToLast(1);
+
+                lastMessageQuery.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                        String data = dataSnapshot.child("message").getValue().toString();
+                        convViewHolder.setMessage(data, conv.isSeen());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                mUsersDatabase.child(listUserId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        final String userEmail = dataSnapshot.child("email").getValue().toString();
+                        String userThumb = dataSnapshot.child("thumbImage").getValue().toString();
+
+                        if(dataSnapshot.hasChild("online")) {
+                            Boolean userOnline = (boolean) dataSnapshot.child("online").getValue();
+                            convViewHolder.setUserOnline(userOnline);
+                        }
+
+                        convViewHolder.setEmail(userEmail);
+                        convViewHolder.setImage(userThumb, getContext());
+
+                        convViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent chatIntent = new Intent(getContext(), ChatActivity.class);
+                                chatIntent.putExtra("user_id", listUserId);
+                                chatIntent.putExtra("user_email", userEmail);
+                                startActivity(chatIntent);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        };
+        mConvList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class ConvViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public ConvViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setMessage(String message, boolean isSeen) {
+            TextView userStatusView = (TextView) mView.findViewById(R.id.statusText);
+            userStatusView.setText(message);
+
+            if(!isSeen) {
+                userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.BOLD);
+
+            } else {
+                userStatusView.setTypeface(userStatusView.getTypeface(), Typeface.NORMAL);
+            }
+        }
+
+        public void setEmail(String email) {
+            TextView userEmailView = (TextView) mView.findViewById(R.id.emailText);
+            userEmailView.setText(email);
+        }
+
+        public void setImage(String thumbImage, Context ctx) {
+            ImageView userImageView = (ImageView) mView.findViewById(R.id.profileImage);
+            Picasso.get().load(thumbImage).placeholder(R.drawable.defaultavatar).into(userImageView);
+        }
+
+        public void setUserOnline(boolean onlineStatus) {
+            ImageView userOnlineView = (ImageView) mView.findViewById(R.id.userSingleOnline);
+
+            if(onlineStatus == true) {
+                userOnlineView.setVisibility(View.VISIBLE);
+            } else {
+                userOnlineView.setVisibility(View.INVISIBLE);
+            }
+        }
+
+    }
+
+
 
 }
